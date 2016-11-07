6e86e44a6b9fff1830b8ad918b3cceea07108b6a
Danilo Cunha <danilobc@gmail.com>
2016-11-07 11:02:44 -0200

    Add minify script an some tests

---
+++ test/test.js
@@ -0,0 +1,16 @@
+var assert = require("assert");
+
+describe('latinize', function() {
+	var latinize;
+  	it('should be successfully initialized', function() {
+    	latinize = require('../latinize.js');
+  	});
+
+   	it('should pass through simple word', function() {
+    	assert.equal(latinize('Test'), 'Test');
+   	});
+
+   	it('should pass through some brazilian portuguese characters', function() {
+    	assert.equal(latinize('Óçãâé'), 'Ocaae');
+   	});
+});
